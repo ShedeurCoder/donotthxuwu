88507b76ba9c64a1948e561c7b9c5a4ab44050c1
ShedeurCoder <81215635+ShedeurCoder@users.noreply.github.com>
2021-05-10 03:30:05 +0800

    Add files via upload

---
+++ JavascriptSkillshare/main.js
@@ -0,0 +1,74 @@
+var pageTitle = "Nico is my senpai"
+document.getElementById('title').innerHTML = pageTitle;
+//owo
+/*
+kasldhgrlaksghev
+*/
+//t i me 
+var day = new Date();
+var hr = day.getHours();
+var greetJs = "Hello!";
+if (hr < 12) {
+    greetJs = "Good Morning!";
+    document.getElementById("greet").innerHTML = greetJs;
+}
+else if (hr == 12) {
+    greetJs = "Good Noon!";
+    document.getElementById("greet").innerHTML = greetJs;
+}
+else if (hr > 12 && hr < 18) {
+    greetJs = "Good Afternoon!";
+    document.getElementById("greet").innerHTML = greetJs;
+}
+else if (hr > 18) {
+    greetJs = "Good Evening!";
+    document.getElementById("greet").innerHTML = greetJs;
+}
+else {
+    document.getElementById("greet").innerHTML = greetJs;
+}
+
+var x = "lolmao";
+if (hr < 12) {
+    x = "nico is the best";
+    document.getElementById("chissy").innerHTML = x;
+}
+else {
+    document.getElementById("chissy").innerHTML = x;
+}
+
+alert("why fart");
+console.log("NICO IS THE BEST!")
+alert("owo");
+alert("NICO IS MY SENPAI!!!!");
+alert("MARRY ME NICO!");
+alert("yes");
+alert("no fart");
+alert("YOU CAN NEVER ESCAPE MEEEEEEEEEEEEEEEEEEE");
+alert("seriously, why fart");
+alert('pls no fart');
+alert('thx uwu');
+alert('Nico di Angelo and Alex Fierro are pogchamp');
+alert("did you just fart?");
+alert('no wet farts pls');
+alert('not in front of Nico and Alex');
+alert('or they will NEVER like me');
+alert('so PLEASE no fart');
+alert('thx');
+alert('uwu');
+alert('btw, if you dont know who Nico and Alex are,');
+alert('Nico di Angelo is a character from Percy Jackson and the Olympians,');
+alert('And I may or may not have a crush on him');
+alert('ok, i have a huge crush on him');
+alert('im not gay i swear');
+alert('cuz im bisexual baby');
+alert('Alex Fierro is a genderfluid character from Magnus Chase and the Gods of Asgard')
+alert('And I also have a crush on him/her');
+alert('k bye for real.');
+
+//x = "i love how the entire fandom is like, 'you must at least like nicos character to be accepted' or 'you must not like octavian or you are going to tartarus'"
+//alert(x);
+
+//var death = 10;
+//death--;
+//alert(death);
